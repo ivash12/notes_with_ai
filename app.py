e185c37be2b0c59ccfe8dff2eed475def33176e9
ivan <iv.shmatko.dev@gmail.com>
2026-06-18 13:50:31 +0200

    pages and .env added

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Study with AI")
 st.subheader("Download the photo of your notes and get the quizes to be able to remember what you have learned")
 
 uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
-    st.image(uploaded_file)
+    image = Image.open(uploaded_file)
+    st.write("Choose the following option")
+    key_concepts = st.button("Key concepts")
+    quizes = st.button("Quizes")
+    if key_concepts:
+        st.switch_page("pages/key_concepts.py")
+    if quizes:
+        st.switch_page("pages/quizes.py")
